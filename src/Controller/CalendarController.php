a28d9da2a57fcb1d39329e49c86d7ec1e92620cb
Yann Malfer <yannmalfer@gmail.com>
2022-02-02 21:47:17 +0100

    css partie calendar ok

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -68,7 +68,7 @@ class CalendarController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'calendar_delete', methods: ['POST'])]
+    #[Route('/delete/{id}', name: 'calendar_delete', methods: ['POST'])]
     public function delete(Request $request, Calendar $calendar, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$calendar->getId(), $request->request->get('_token'))) {
